30086613f86bf2fd5bcf9058d2bcbab58d205ede
Ana Lucía <ana.duran@cgr.go.cr>
2019-07-25 18:16:31 +0200

    Thusday w3d4
    Iris, Wine, Boston and Titanic

---
+++ Week 3/Day 2 - Hypothesis/prueba.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import statsmodels.api as sm
+da = pd.read_csv("nhanes_2015_2016.csv")
+
+
+da.head()
+
